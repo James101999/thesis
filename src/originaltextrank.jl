3d7f10cd23789ee2aa1ea7e0e59c112db7750ce0
James <mrnjmshernandez@gmail.com>
2022-04-01 18:02:18 +0800

    Fixed another set of type stabilities. Integrated spacy's neuralcoref (haven't plugged it in the main textrank algorithm because of its computational overhead. Also, created a new directory for the datasets.

--- src/originaltextrank.jl
+++ src/originaltextrank.jl
@@ -1,65 +1,45 @@
 using WordTokenizers
 using TextAnalysis
 using TextModels
-using Languages
 using Graphs
 using PyCall
+using CSV
+using DataFrames
 
 # Neural-based PoS Tagger consuming too much memory
 pos = PoSTagger()
 
-# This is just temporary, will move this test data soon
-function data()
-    """Statistical expert / data scientist / analytical developer
-    BNOSAC (Belgium Network of Open Source Analytical Consultants), is a Belgium consultancy company specialized in data analysis and statistical consultancy using open source tools.
-    In order to increase and enhance the services provided to our clients, we are on the lookout for an all-round statistical expert, data scientist and analytical developer.
-    Function:
-    Your main task will be the execution of a diverse range of consultancy services in the field of statistics and data science.
-    You will be involved in a small team where you handle the consultancy services from the start of the project until the end.
-    This covers:
-    Joint meeting with clients on the topic of the analysis.
-    Acquaintance with the data.
-    Analysis of the techniques that are required to execute the study.
-    Mostly standard statistical and biostatistical modelling, predictive analytics & machine learning techniques.
-    Perform statistical design, modeling and analysis, together with more seniors.
-    Building the report on the data analysis.
-    Automating and R/Python package development.
-    Integration of the models into the existing architecture.
-    Giving advise to the client on the research questions, design or integration.
-    Next to that, you will help in building data products and help sell them.
-    These cover text mining, integration of predictive analytics in existing tools and the creation of specific data analysis tools and web services.
-    You also might be involved in providing data science related courses for clients.
-    Profile:
-    You have a master degree in the domain of Statistics, Biostatistics, Mathematics, Commercial or Industrial Engineering, Economics or similar.
-    You have a strong interest in statistics and data analysis.
-    You have good communication skills, are fluent in English and know either Dutch or French.
-    You soak up new knowledge and either just make things work or have the attitude of 'I can do this'.
-    Besides this, you have attention to detail and adapt to changes quickly.
-    You have programming experience in R or you really want to switch to using R.
-    You have a sound knowledge of another data analysis language (Python, SQL, javascript) and you don't care in which relational database, Excel, bigdata or noSQL store your data is located.
-    Interested in robotics is a plus.
-    Offer:
-    A half or full-time employment depending on your personal situation.
-    The ability to get involved in a whole range of sectors and topics and the flexibility to shape your own future.
-    The usage of a diverse range of statistical & data science techniques.
-    Support in getting up to speed quickly in the usage of R.
-    An environment in which you can develop your talent and make your own proposals the standard way to go.
-    Liberty in managing your open source projects during working hours.
-    Contact:
-    To apply or in order to get more information about the job content, please contact us at: http://bnosac.be/index.php/contact/get-in-touch"""
-end
-
 # Corpus and other CSV will go here soon
 
-function lemmatization(data)
-    pyimport("spacy")
-    whose(spacy)
-    #nlp = spacy.load("en_core_web_sm")
-    #lemmatizer = nlp.get_pipe("lemmatizer")
-    #doc = nlp(data)
-    #[token.lemma_ for token in doc].join(' ')
-end
+# Call python libraries here
+#function lemmatization(data)
+#    py"""
+#    import spacy
+#    nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
+#    
+#    def lemmatize(x):
+#        doc = nlp(x)
+#        return " ".join([token.lemma_ for token in doc])
+#    """
+#    py"lemmatize"(data)
+#end
 
+function coreference(data)
+    py"""
+    # src code: from huggingface's neuralcoref
+    import spacy
+    nlp = spacy.load('en')
+
+    import neuralcoref
+    neuralcoref.add_to_pipe(nlp)
+
+    def pass_data(x):
+        doc = nlp(x)
+        return doc._.coref_resolved
+    """
+    py"pass_data"(data)
+end
+    
 function postagging(data)
     # Tokenize the text
     #tok = tokenize(text(sd))
@@ -98,7 +78,7 @@ end
 
 function cooccurrencematrix(corpus)
     # Build a co-occurrence matrix of words
-    CooMatrix(corpus, window = 3, normalize = false)
+    CooMatrix(corpus, window = 2, normalize = false)
 end
 
 function buildgraph(cooccurrencematrix)
@@ -142,21 +122,21 @@ function postprocessing(data)
     phrases = collect(Set(newwords))
     final = []
     for i in eachindex(phrases)
-        splittedphrase = split(phrases[i]::String)
-        tempscore::Integer = 0
+        splittedphrase = split(phrases[i]::AbstractString)
+        tempscore::AbstractFloat = 0.0
         if length(splittedphrase) < 2
             push!(final, (toprankscores[findall(x->x==phrases[i], 
-            toprankwords)[1]::Integer], phrases[i]::String))
+            toprankwords)[1]::Integer], phrases[i]::AbstractString))
         else
             for j in eachindex(splittedphrase)
                 tempscore += toprankscores[findall(x->x==splittedphrase[j], 
                 toprankwords)[1]::Integer]
             end
-            push!(final, (tempscore, phrases[i]::String))
+            push!(final, (tempscore, phrases[i]::AbstractString))
             tempscore = 0
         end
     end
-    map(x -> x[2], sort(final; rev=true))::Vector{String}
+    map(x -> x[2], sort(final; rev=true))::Vector{SubString{String}}
 
 
     #joinwhitespace(x) = join(x, " ")
