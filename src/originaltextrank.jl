63e35639e5490b646d42b446a1d5d89d376f99fd
James <mrnjmshernandez@gmail.com>
2022-04-17 13:42:10 +0800

    Replaced pos tagger with nltk, will soon start testing the performance metrics

--- src/originaltextrank.jl
+++ src/originaltextrank.jl
@@ -5,24 +5,16 @@ using Graphs
 using PyCall
 using CSV
 using DataFrames
+using Metrics
+
+@pyimport nltk
+@pyimport datasets
 
 # Neural-based PoS Tagger consuming too much memory
-pos = PoSTagger()
+# Replaced with nltk pos tagger
+# pos = PoSTagger()
 
 # Corpus and other CSV will go here soon
-
-# Call python libraries here
-#function lemmatization(data)
-#    py"""
-#    import spacy
-#    nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
-#    
-#    def lemmatize(x):
-#        doc = nlp(x)
-#        return " ".join([token.lemma_ for token in doc])
-#    """
-#    py"lemmatize"(data)
-#end
 
 function coreference(data)
     py"""
@@ -39,41 +31,60 @@ function coreference(data)
     """
     py"pass_data"(data)
 end
-    
-function postagging(data)
+
+#function postagging(data)
     # Tokenize the text
-    #tok = tokenize(text(sd))
-    tok = split_sentences(data)
+    #tok = nltk_word_tokenize(data)
+    #tok = [x[1:end-1] for x in split_sentences(data)]
     # Tag with parts of speech
     #pos = PoSTagger()::TextModels.PoSModel
-    #tags = pos(text(sd)::String)::Vector{String}
-    tags = pos.(tok)::Vector{Vector{String}}
-    # unfiltered = collect(zip(tokenizer::Vector{String}, tags::Vector{String}))
-    zipped = [collect(zip(nltk_word_tokenize(tok[i])::Vector{String}, tags[i]::Vector{String})) for i in eachindex(tok)]
-    reduce(vcat, zipped)
-end
+    #tags = pos(data::String)::Vector{String}
+    #tags = pos.(tok)::Vector{Vector{String}}
+    #collect(zip(tok::Vector{String}, tags::Vector{String}))
+    #zipped = [collect(zip(nltk_word_tokenize(tok[i])::Vector{String}, tags[i]::Vector{String})) for i in eachindex(tok)]
+    #reduce(vcat, zipped)
+#end
 
-function documentpreparation(data)
+function documentpreparation(data::String)
     sd = StringDocument(data)
     # Let us first remove the unreadable characters
     remove_corrupt_utf8!(sd)
     # lower case every word
-    remove_case!(sd)
+    #remove_case!(sd)
     # Too aggressive, replace with lemmatization
     # stem!(sd)
     sd
 end
 
-function preprocessing(document)
+    
+function preprocessing(document::String)
     sd = documentpreparation(document)
+    tok = nltk_word_tokenize(text(sd))
      # stripping articles and stopwords
-    prepare!(sd, strip_articles | strip_stopwords)
-    tags = postagging(text(sd))
+    #prepare!(sd, strip_articles | strip_stopwords)
+    pytok = PyVector(tok)
+    tags = convert(Vector{Tuple{String, String}}, pycall(nltk.pos_tag, PyVector{Tuple{String, String}}, pytok))
     # Filter tagged vocabulary
     filtered = filter(t -> ((_, y) = t;
         y == "NN" || y == "NNS" || y == "NNP" || y == "NNPS"
             || y == "JJ" || y == "JJR" || y == "JJS"), tags)
-    Corpus([TokenDocument([x for (x, _) in filtered])])
+    joined = TokenDocument([x for (x, _) in filtered])
+    remove_case!(joined)
+    Corpus([joined])
+end
+
+function preprocessing(document::Vector)
+     # stripping articles and stopwords
+    #prepare!(sd, strip_articles | strip_stopwords)
+    pydoc = PyVector(document)
+    tags = convert(Vector{Tuple{String, String}}, pycall(nltk.pos_tag, PyVector{Tuple{String, String}}, pydoc))
+    # Filter tagged vocabulary
+    filtered = filter(t -> ((_, y) = t;
+        y == "NN" || y == "NNS" || y == "NNP" || y == "NNPS"
+            || y == "JJ" || y == "JJR" || y == "JJS"), tags)
+    joined = TokenDocument([x for (x, _) in filtered])
+    remove_case!(joined)
+    Corpus([joined])
 end
 
 function cooccurrencematrix(corpus)
@@ -94,14 +105,15 @@ function score(data)
     # An efficient built in implementation from Graphs.jl
     # Saw another more efficient implementation that uses BLAS, will look into it soon
     score = pagerank(graphmatrix, 0.85, 30, 1.0e-4)::Vector{Float64} # this may cause an error on non-64 bit systems
-    sort(collect(zip(score, coomatrix.terms)); rev = true)[1:floor(Int, 1/3 * length(coomatrix.terms))]
+    sort(collect(zip(score, coomatrix.terms)); rev = true)[1:ceil(Integer, 1/3 * length(coomatrix.terms))]
     # map(x -> x[2], sortedScore)[1:floor(Int, 1/3 * length(coomatrix.terms))]
 end
 
-function postprocessing(data)
+function postprocessing(data::String)
     scores = score(data)
     document = documentpreparation(data)
     prepare!(document, strip_punctuation)
+    remove_case!(document)
     lex = nltk_word_tokenize(text(document))
     toprankwords = [x[2] for x in scores]
     toprankscores = [x[1] for x in scores]
@@ -143,5 +155,51 @@ function postprocessing(data)
     #phrases = map(joinwhitespace, collect(Iterators.product(cooccurrencematrix.terms)))
     #occuringphrase(x) = occursin(x, text(processeddocument))
     #filtered = filter(occuringphrase, phrases)
+
+end
+
+function postprocessing(data::Vector)
+    scores = score(data)
+    toprankwords = [x[2] for x in scores]
+    toprankscores = [x[1] for x in scores]
+    newwords = []
+    multiword = ""
+
+    for i in eachindex(data)
+        if lex[i] ∈ toprankwords
+            multiword *= lex[i] * " "
+        else
+            if occursin(multiword, text(document)) && !isempty(multiword)
+                push!(newwords, rstrip(multiword))
+            end
+            multiword = ""
+        end
+    end
+
+    phrases = collect(Set(newwords))
+    final = []
+    for i in eachindex(phrases)
+        splittedphrase = split(phrases[i]::AbstractString)
+        tempscore::AbstractFloat = 0.0
+        if length(splittedphrase) < 2
+            push!(final, (toprankscores[findall(x->x==phrases[i], 
+            toprankwords)[1]::Integer], phrases[i]::AbstractString))
+        else
+            for j in eachindex(splittedphrase)
+                tempscore += toprankscores[findall(x->x==splittedphrase[j], 
+                toprankwords)[1]::Integer]
+            end
+            push!(final, (tempscore, phrases[i]::AbstractString))
+            tempscore = 0
+        end
+    end
+    map(x -> x[2], sort(final; rev=true))::Vector{SubString{String}}
+end
+
+function simulation()
+    dataset = datasets.load_dataset("midas/semeval2010", "raw")
+    test_data = dataset["test"]
+    alldocuments = test_data["document"]
+    goldenkeys = test_data["extractive_keyphrases"]
 
 end
